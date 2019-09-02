88c5299ecf2091aa916032d868d084c1d86dfcc1
vishalmurali92 <vishal92.murali@gmail.com>
2019-09-02 13:28:26 -0700

    Starting Modification of twist_controller.py

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -4,9 +4,11 @@ ONE_MPH = 0.44704
 
 
 class Controller(object):
-    def __init__(self, *args, **kwargs):
-        # TODO: Implement
-        pass
+    def __init__(self,vehicle_mass,fuel_capacity,brake_deadband,decel_limit,
+        accel_limit,wheel_radius,wheel_base,steer_ratio,max_lat_accel,max_steer_angle):
+
+        self.yaw_controller = YawController(wheel_base,steer_ratio,0.1,max_lat_accel,max_steer_angle)
+        
 
     def control(self, *args, **kwargs):
         # TODO: Change the arg, kwarg list to suit your needs
